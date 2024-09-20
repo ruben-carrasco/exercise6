d202bfe72565aadba8133dd13aea5d1bf1eb38b0
ruben.carrasco <ruben.carrasco@106-05.info2>
2024-09-20 14:06:27 +0200

    Exercise6 terminado

---
+++ Exercise6/src/exercise6/Exercise6.java
@@ -0,0 +1,12 @@
+package exercise6;
+
+public class Exercise6 {
+	public static void main(String[] args) {
+		System.out.println("Rubén");
+		System.out.println("Avenida de las Aves 16");
+		System.out.println();
+		System.out.println("695975462");
+		
+	}
+
+}
